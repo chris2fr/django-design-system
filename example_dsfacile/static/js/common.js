0d04df16d9134cac7a1481cb481b96e1dcd18453
Chris Mann <chris@lesgtrandsvoisins.com>
2024-05-07 21:05:15 +0200

    .fr- to .dsfacile-

--- example_dsfacile/static/js/common.js
+++ example_dsfacile/static/js/common.js
@@ -1,4 +1,4 @@
-const language_selectors = document.querySelectorAll(".fr-translate__language")
+const language_selectors = document.querySelectorAll(".dsfacile-translate__language")
 
 language_selectors.forEach(el => el.addEventListener("click", event => {
     document.cookie = "django_language=" + el.lang + ";Path=\"/django-dsfacile\";SameSite=Strict"
