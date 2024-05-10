024247741f1a87c51388e17a5d43a0d358ff3e42
Chris Mann <chris@lesgtrandsvoisins.com>
2024-05-10 13:00:40 +0200

    Published as a module

--- django_webfastoche/webfastoche_lesgrandsvoisins/apps.py
+++ django_webfastoche/webfastoche_lesgrandsvoisins/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class WebfastocheConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "webfastoche_lesgrandsvoisins"
+    name = "django_webfastoche.webfastoche_lesgrandsvoisins"
     verbose_name = "Système de design générique sur la base de celui de l’État français"
