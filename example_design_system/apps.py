88ab00a9a9c7df94fae723c6a70e966f40bf35c1
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-10 15:31:50 +0100

    Eliminated Example Design System

--- example_design_system/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-from django.utils.translation import gettext_lazy as _
-
-
-class ExampleDjangoDesignSystemConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"  # type: ignore
-    name = "example_design_system"
-    verbose_name = _("Example design_system")
--- example_design_system/static/design-system/js/common.js
+++
@@ -1,6 +0,0 @@
-const language_selectors = document.querySelectorAll(".design-system-translate__language")
-
-language_selectors.forEach(el => el.addEventListener("click", event => {
-    document.cookie = "django_language=" + el.lang + ";Path=\"/django-design-system\";SameSite=Strict"
-    window.location.reload()
-}));
